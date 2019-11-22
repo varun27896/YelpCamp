1e7a4833a0ac6483763e2757c572dd4d6c90cbed
varun27896 <varun27896@gmail.com>
2019-11-22 17:54:29 -0600

    Added functionality to create campgrounds, navbar to all templates, styled the new campground form

--- v1/app.js
+++ v1/app.js
@@ -1,7 +1,18 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
+
 
 var port = process.env.PORT || 3000;
+
+var campgrounds = [
+  {name:"Varun Chillara", image:"https://pixabay.com/get/55e9d043485baa14f6da8c7dda793f7f1636dfe2564c704c722b78dd924ccc5a_340.jpg"},
+  {name:"Varun Shravan", image:"https://pixabay.com/get/57e6d7454e53ae14f6da8c7dda793f7f1636dfe2564c704c722b78dd924ccc5a_340.jpg"},
+  {name:"Shravan Varun", image:"https://pixabay.com/get/57e8dc414e5ba814f6da8c7dda793f7f1636dfe2564c704c722b78dd924ccc5a_340.jpg"}
+];
+
+
+app.use(bodyParser.urlencoded({extended:true}));
 
 //enables filenames to be included without .ejs in render method
 app.set("view engine", "ejs");
@@ -14,14 +25,26 @@ app.get("/", function(req, res){
 });
 
 app.get("/campgrounds", function(req, res){
-  var campgrounds = [
-    {name:"Varun Chillara", image:"https://pixabay.com/get/55e9d043485baa14f6da8c7dda793f7f1636dfe2564c704c722b78dd924ccc5a_340.jpg"},
-    {name:"Varun Shravan", image:"https://pixabay.com/get/57e6d7454e53ae14f6da8c7dda793f7f1636dfe2564c704c722b78dd924ccc5a_340.jpg"},
-    {name:"Shravan Varun", image:"https://pixabay.com/get/57e8dc414e5ba814f6da8c7dda793f7f1636dfe2564c704c722b78dd924ccc5a_340.jpg"}
-  ]
+
   res.render("campgrounds", {campgrounds:campgrounds});
 });
 
+app.post("/campgrounds", function(req, res){
+  
+  //get data from from and add to campgrounds array
+  var name = req.body.name;
+  var image = req.body.image;
+  var newCampground = {name:name, image:image};
+  campgrounds.push(newCampground);
+//redirect to campgrounds page
+res.redirect("/campgrounds");
+});
+
+
+app.get("/campgrounds/new", function(req, res){
+  res.render("new");
+
+});
 
 
 
